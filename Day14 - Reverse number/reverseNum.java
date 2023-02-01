f49223d5336f5fb2c2b577c5a13ea0a6da0b97ee
Abidarsh mohan <79636592+abidarshmohan@users.noreply.github.com>
2023-02-01 15:36:36 +0530

    Add files via upload

---
+++ Day14 - Reverse number/reverseNum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class reverseNum{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);      
+        System.out.println("Enter the number");
+        int num =  sc.nextInt();
+        int rev=0,temp;
+       while(num!=0)
+       {
+        temp=num%10;
+        rev=rev*10+temp;
+        num=num/10;
+       }
+        System.out.println(rev);
+    }
+    
+}
