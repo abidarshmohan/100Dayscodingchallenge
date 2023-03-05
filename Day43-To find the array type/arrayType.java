cfeb6e493873386aacc7507ecea2a19e712cf66d
Abidarsh mohan <79636592+abidarshmohan@users.noreply.github.com>
2023-03-05 23:23:33 +0530

    Add files via upload

---
+++ Day43-To find the array type/arrayType.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+
+public class arrayType {
+    public static void main(String[] args) {
+        Scanner sc=new  Scanner (System.in);
+        System.out.println("enter the array size : ");
+        int arrSize =sc.nextInt();
+         
+        System.out.println("enter the elements");
+        int[] arr= new int[arrSize];
+        for(int i=0;i<arrSize;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int count=0,flag=0;
+        for(int i=0;i<arrSize;i++)
+        {
+          if(arr[i] %2 ==1)
+          count++;
+          else
+          flag++;
+            
+        }
+        if(count==arrSize){
+            System.out.println("odd");
+        }
+    else if(flag==arrSize)
+    {
+        System.out.println( "Even");
+    }
+    else{
+    System.out.println("mixed");
+    }
+        
+        
+
+    }
+}
