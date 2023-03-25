4fd7e2ca64fd9570490113728c21a82834301a5a
Abidarsh mohan <79636592+abidarshmohan@users.noreply.github.com>
2023-03-25 22:30:49 +0530

    Add files via upload

---
+++ Day58-Bucket filling/main1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main1 {
+    public static void main(String[] args) throws java.lang.Exception {
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        int[] k = new int[n];
+        int[] x = new int[n];
+        int[] r = new int[n];
+        int i;
+        for (i = 0; i < n; i++) {
+            k[i] = sc.nextInt();
+            x[i] = sc.nextInt();
+            r[i] = k[i] - x[i];
+        }
+        for (i = 0; i < n; i++) {
+            System.out.println(r[i] = k[i] - x[i]);
+        }
+    }
+}
