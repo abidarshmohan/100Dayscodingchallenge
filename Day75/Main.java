e43157425a76735f153f974f82f1a1214d87306b
Abidarsh mohan <79636592+abidarshmohan@users.noreply.github.com>
2023-04-13 23:39:39 +0530

    Add files via upload

---
+++ Day75/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Main
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            if(n%k==0)
+            System.out.println(n/k+"\n");
+            else
+            System.out.println("-1\n");
+        }
+    }
+}
